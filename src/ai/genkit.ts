61eb3cd7eff6335f4ad0a1b0650dd5f588aac2c0
ganondorofu <167788682+ganondorofu@users.noreply.github.com>
2025-07-02 04:46:19 +0000

    Vercelでデプロイしたらai要約でエラーが出る

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -2,6 +2,6 @@ import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
 
 export const ai = genkit({
-  plugins: [googleAI()],
+  plugins: [googleAI({apiKey: "AIzaSyCpdBXQ6p_xFGf1skCYNtxInCFLK20s9JU"})],
   model: 'googleai/gemini-2.0-flash',
 });
